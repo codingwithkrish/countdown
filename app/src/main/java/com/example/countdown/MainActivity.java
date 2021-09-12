5449b7fff8fc0b19df761917bb869eb36927a3cc
Krish Gupta <krishgupta.8.kg@gmail.com>
2021-09-12 16:47:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/countdown/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.countdown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    Button start,stop,reset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chronometer = findViewById(R.id.chronometer);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+        reset = findViewById(R.id.reset);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.start();
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.stop();
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+
+    }
+}
